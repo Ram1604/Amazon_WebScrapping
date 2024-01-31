e171f0a0edc3e717af8c07a019207567b4a80881
Ramesh <rameshsudhan11@gamil.com>
2024-01-31 10:46:32 +0530

    changes added

--- WS.py
+++ WS.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 #capturing HTML from webpage
 headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.'} 
-URL = "https://www.amazon.in/Apple-iPhone-15-256-GB/dp/B0CHX6N27Y/ref=sr_1_1_sspa?crid=1UHQHOQNPDPLR&keywords=apple+15&qid=1706520727&sprefix=apple+15%2Caps%2C434&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
+URL = "https://www.amazon.in/dp/B0CHX3RP9R?ref=ods_ucc_kindle_B0CHX3RP9R_nrc_ucc"
 r = requests.get(URL, headers=headers)
 #print(r.text)
 print(r.status_code)
@@ -19,3 +19,7 @@ print(title)
 price_element = soup.select_one('.a-price-whole')
 price = price_element.text.strip()
 print(price)
+
+with open('output.txt', 'w') as file:
+    # Write output to the file
+    file.write(title + '  -  ' + price)
