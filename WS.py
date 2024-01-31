e89298d7b772af017159b6afaa06f4be19203262
Ramesh <rameshsudhan11@gamil.com>
2024-01-31 15:28:07 +0530

    Changes update

--- WS.py
+++ WS.py
@@ -1,16 +1,16 @@
+#Importing Libraries
 import requests 
 from bs4 import BeautifulSoup
 
-#capturing HTML from webpage
+#Capturing HTML from Webpage
 headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.'} 
 URL = "https://www.amazon.in/dp/B0CHX3RP9R?ref=ods_ucc_kindle_B0CHX3RP9R_nrc_ucc"
 r = requests.get(URL, headers=headers)
-#print(r.text)
 print(r.status_code)
 
 #prasing content
 soup = BeautifulSoup(r.text, 'lxml')
-#print(soup.prettify())
+print(soup.prettify())
 
 title_element = soup.select_one('#productTitle')
 title = title_element.text.strip()
